6d0e91351e7ac11655a525a2a897b064b8db2cb7
piyushraaj <49487709+piyushraaj@users.noreply.github.com>
2019-04-13 22:45:50 +0530

    Add files via upload

--- queue1.cpp
+++ queue1.cpp
@@ -59,4 +59,26 @@ int main()
       printf("\n\nAverage Waiting Time:\t%f", average_wait_time); 
       printf("\nAvg Turnaround Time:\t%f\n", average_turnaround_time); 
       return 0; 
-}
+
+    int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
+    printf("Enter total number of processes(maximum 20):");
+    scanf("%d",&n);
+ 
+    printf("\nEnter Process Burst Time\n");
+    for(i=0;i<n;i++)
+    {
+        printf("P[%d]:",i+1);
+        scanf("%d",&bt[i]);
+    }
+ 
+    wt[0]=0;    //waiting time for first process is 0
+ 
+    //calculating waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+ 
+    printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
